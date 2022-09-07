b389d08cafe2bc8cd933385061167b6d5042a0ac
rachelhavas <rachel.havas2@gmail.com>
2022-09-07 12:21:02 -0400

    added controller tests

--- src/budget-items/budget-items.controller.spec.ts
+++ src/budget-items/budget-items.controller.spec.ts
@@ -1,20 +1,117 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateBudgetItemDto } from './dto/create-budget-item.dto';
 import { BudgetItemsController } from './budget-items.controller';
 import { BudgetItemsService } from './budget-items.service';
 
+const createBudgetItemDto: CreateBudgetItemDto = {
+  id: 1,
+  name: 'name1',
+  amount: '100.00',
+  owner: 'owner1',
+  startDate: '01/01/2022',
+  endDate: '01/01/2023',
+  isActive: true,
+};
+
 describe('BudgetItemsController', () => {
-  let controller: BudgetItemsController;
+  let budgetItemsController: BudgetItemsController;
+  let budgetItemsService: BudgetItemsService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const app: TestingModule = await Test.createTestingModule({
       controllers: [BudgetItemsController],
-      providers: [BudgetItemsService],
+      providers: [
+        BudgetItemsService,
+        {
+          provide: BudgetItemsService,
+          useValue: {
+            create: jest
+              .fn()
+              .mockImplementation((budgetItem: CreateBudgetItemDto) =>
+                Promise.resolve({ id: 1, ...budgetItem }),
+              ),
+            findAll: jest.fn().mockResolvedValue([
+              {
+                id: 1,
+                name: 'name1',
+                amount: '100.00',
+                owner: 'owner1',
+                startDate: '01/01/2022',
+                endDate: '01/01/2023',
+                isActive: true,
+              },
+              {
+                id: 2,
+                name: 'name2',
+                amount: '200.00',
+                owner: 'owner2',
+                startDate: '01/01/2022',
+                endDate: '01/01/2023',
+                isActive: true,
+              },
+            ]),
+            findOne: jest.fn().mockImplementation((id: number) =>
+              Promise.resolve({
+                id: 1,
+                name: 'name1',
+                amount: '100.00',
+                owner: 'owner1',
+                startDate: '01/01/2023',
+                endDate: '01/01/2023',
+                isActive: true,
+              }),
+            ),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<BudgetItemsController>(BudgetItemsController);
+    budgetItemsController = app.get<BudgetItemsController>(BudgetItemsController);
+    budgetItemsService = app.get<BudgetItemsService>(BudgetItemsService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(budgetItemsController).toBeDefined();
+  });
+
+  describe('create()', () => {
+    it('should create a budget item', () => {
+      budgetItemsController.create(createBudgetItemDto);
+      expect(budgetItemsController.create(createBudgetItemDto)).resolves.toEqual({
+        id: 1,
+        ...createBudgetItemDto,
+      });
+      expect(budgetItemsService.create).toHaveBeenCalledWith(createBudgetItemDto);
+    });
+  });
+
+  describe('findAll()', () => {
+    it('should find all budget items ', () => {
+      budgetItemsController.findAll();
+      expect(budgetItemsService.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne()', () => {
+    it('should find a budget item', () => {
+      expect(budgetItemsController.findOne(1)).resolves.toEqual({
+        id: 1,
+        name: 'name1',
+        amount: '100.00',
+        owner: 'owner1',
+        startDate: '01/01/2023',
+        endDate: '01/01/2023',
+        isActive: true,
+      });
+      expect(budgetItemsService.findOne).toHaveBeenCalled();
+    });
+  });
+
+  describe('remove()', () => {
+    it('should remove the budget item', () => {
+      budgetItemsController.remove(1);
+      expect(budgetItemsService.remove).toHaveBeenCalled();
+    });
   });
 });
